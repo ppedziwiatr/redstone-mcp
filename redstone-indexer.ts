3f884e6da59da886e0b84cd8939bad28c3accb4c
ppedziwiatr <ppedziwiatr@gmail.com>
2025-06-13 11:40:53 +0200

    indexer

--- redstone-indexer.ts
+++ redstone-indexer.ts
@@ -1,30 +1,39 @@
 import {DenoKVPriceDatabase} from "./src/storage/kv-storage.ts";
 import {fetchPriceFeeds} from "./src/storage/redstone.ts";
 
-async function loadPrices(lastTimestamp: number, db: DenoKVPriceDatabase) {
-    try {
-        console.log("Load feeds");
-        const mappedData = await fetchPriceFeeds();
-        const newTimestamp = mappedData["ETH"]!.timestamp;
-        console.log(`Timestamps: current ${newTimestamp}, last ${lastTimestamp}`)
-        if (newTimestamp !== lastTimestamp) {
-            console.log("Storing data");
-        } else {
-            console.log(`Timestamps the same (current ${newTimestamp}, last ${lastTimestamp})`);
-        }
-        await db.storePriceData(mappedData);
-        lastTimestamp = newTimestamp;
-    } catch (e) {
-        console.error(e);
-    }
-}
-
 if (import.meta.main) {
+    globalThis.addEventListener("beforeunload", async (): Promise<void> => {
+        await db.close();
+    });
+
+    Deno.addSignalListener("SIGINT", async (): Promise<void> => {
+        await db.close();
+        Deno.exit(0);
+    });
+
     const db = await DenoKVPriceDatabase.create("./redstone_data.db", 30);
     let lastTimestamp = 0;
 
     setInterval(async () => {
-        await loadPrices(lastTimestamp, db);
+        await loadPrices();
     }, 10100);
-    await loadPrices(lastTimestamp, db);
+    await loadPrices();
+
+    async function loadPrices() {
+        try {
+            console.log("Load feeds");
+            const mappedData = await fetchPriceFeeds();
+            const newTimestamp = mappedData["ETH"]!.timestamp;
+            console.log(`Timestamps: current ${newTimestamp}, last ${lastTimestamp}`)
+            if (newTimestamp !== lastTimestamp) {
+                console.log("Storing data");
+            } else {
+                console.log(`Timestamps the same (current ${newTimestamp}, last ${lastTimestamp})`);
+            }
+            await db.storePriceData(mappedData);
+            lastTimestamp = newTimestamp;
+        } catch (e) {
+            console.error(e);
+        }
+    }
 }
